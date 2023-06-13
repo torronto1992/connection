104cc68d78a2e271a05455216b3c85e42b7ea725
torronto1992 <136285385+torronto1992@users.noreply.github.com>
2023-06-14 02:44:01 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt update -y
+
+sudo apt install -y python3-pip
+
+pip3 install prettytable
+pip3 install ping3
+pip3 install selenium
+pip3 install telebot
+pip3 install apscheduler
+pip3 install flask
+pip3 install flask_cors
+pip3 install fpdf2
+pip3 install jsonpickle
+pip3 install BeautifulSoup4
+pip3 install pandas
+pip3 install requests
+
+sudo apt install -y unzip
+
+sudo apt install -y npm
+sudo npm install -g pm2
